8594893f4573595f44ebe60f751d9c3962dcd1ee
madiodio <papigaye1@gmail.com>
2018-07-14 17:16:39 +0000

    🦋

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import MapWithADirectionsRenderer from "./components/MapComponent";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <MapWithADirectionsRenderer />;
+  }
+}
+
+export default App;
